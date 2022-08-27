0f2d443359d163c2cde0edab78dc4ac88ea15305
0-harshit-0 <59227793+0-harshit-0@users.noreply.github.com>
2022-08-27 14:14:00 +0530

    added top panel

---
+++ wysiwyg/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Top from './components/top/top.jsx';
+
+function App() {
+  return (
+    <div className="container">
+      <Top />
+    </div>
+  );
+}
+
+export default App;
---
+++ wysiwyg/src/components/top/top.jsx
@@ -0,0 +1,37 @@
+import './top.css';
+
+function Top() {
+  return (
+    <div className="top">
+      <button className="menuIconBtn">
+        <svg viewBox="0 0 100 80" width="20" height="20" fill="rgba(150, 150, 150, 1)">
+          <rect width="100" height="15"></rect>
+          <rect y="30" width="100" height="15"></rect>
+          <rect y="60" width="100" height="15"></rect>
+        </svg>
+      </button>
+
+      <input className="topSearch" type="search" name="search" placeholder="search the document" />
+
+      <button className="inviteBtn">
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-person-plus-fill" viewBox="0 0 16 16">
+          <path d="M1 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
+          <path fillRule="evenodd" d="M13.5 5a.5.5 0 0 1 .5.5V7h1.5a.5.5 0 0 1 0 1H14v1.5a.5.5 0 0 1-1 0V8h-1.5a.5.5 0 0 1 0-1H13V5.5a.5.5 0 0 1 .5-.5z"/>
+        </svg>
+        INVITE TEAM MEMBER
+      </button>
+
+      <button className="notiIconBtn">
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="rgba(150, 150, 150, 1)" className="bi bi-bell" viewBox="0 0 16 16">
+          <path d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2zM8 1.918l-.797.161A4.002 4.002 0 0 0 4 6c0 .628-.134 2.197-.459 3.742-.16.767-.376 1.566-.663 2.258h10.244c-.287-.692-.502-1.49-.663-2.258C12.134 8.197 12 6.628 12 6a4.002 4.002 0 0 0-3.203-3.92L8 1.917zM14.22 12c.223.447.481.801.78 1H1c.299-.199.557-.553.78-1C2.68 10.2 3 6.88 3 6c0-2.42 1.72-4.44 4.005-4.901a1 1 0 1 1 1.99 0A5.002 5.002 0 0 1 13 6c0 .88.32 4.2 1.22 6z"/>
+        </svg>
+      </button>
+      
+      <button className="userIconBtn">
+        <img src={require('./unnamed.png')} loading="lazy" decoding="sync" />
+      </button>
+    </div>
+  );
+}
+
+export default Top;
