4505f5226e75943ef5f71b98d0e2d23bac08e5d9
0-harshit-0 <59227793+0-harshit-0@users.noreply.github.com>
2022-08-28 12:19:50 +0530

    top changes and left started

--- wysiwyg/src/App.js
+++ wysiwyg/src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import Top from './components/top/top.jsx';
-import Top from './components/left/left.jsx';
+import Left from './components/left/left.jsx';
 
 function App() {
   return (
     <div className="container">
       <Top />
+      <Left />
     </div>
   );
 }
--- wysiwyg/src/components/top/top.jsx
+++ wysiwyg/src/components/top/top.jsx
@@ -5,17 +5,17 @@ import './top.css';
 function TopMenu(props) {
   return(
     <section className="topMenu">
-      <button onClick={()=> {props.setDrawerTop(props.drawerTop > 1 ? -5 : 80)}} className="menuIconBtn hovers">
+      <button onClick={()=> {props.setDrawerTop(props.drawerTop > 35 ? 35 : 80)}} className="menuIconBtn tophovers">
         <svg viewBox="0 0 100 80" width="20" height="20" fill="currentColor">
-          <rect width="100" height="15"></rect>
-          <rect y="30" width="100" height="15"></rect>
-          <rect y="60" width="100" height="15"></rect>
+          <rect y="20" width="100" height="10"></rect>
+          <rect y="45" width="100" height="10"></rect>
+          <rect y="70" width="100" height="10"></rect>
         </svg>
       </button>
 
-      <input className="topSearch hovers" type="search" name="search" placeholder="search the document" />
+      <input className="topSearch tophovers" type="search" name="search" placeholder="search the document" />
 
-      <button className="inviteBtn hovers">
+      <button className="topinviteBtn tophovers">
         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-person-plus-fill" viewBox="0 0 16 16">
           <path d="M1 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
           <path fillRule="evenodd" d="M13.5 5a.5.5 0 0 1 .5.5V7h1.5a.5.5 0 0 1 0 1H14v1.5a.5.5 0 0 1-1 0V8h-1.5a.5.5 0 0 1 0-1H13V5.5a.5.5 0 0 1 .5-.5z"/>
@@ -23,13 +23,13 @@ function TopMenu(props) {
         INVITE TEAM MEMBER
       </button>
 
-      <button className="notiIconBtn hovers">
+      <button className="topnotiIconBtn tophovers">
         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-bell-fill" viewBox="0 0 16 16">
           <path d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2zm.995-14.901a1 1 0 1 0-1.99 0A5.002 5.002 0 0 0 3 6c0 1.098-.5 6-2 7h14c-1.5-1-2-5.902-2-7 0-2.42-1.72-4.44-4.005-4.901z"/>
         </svg>
       </button>
       
-      <button className="userIconBtn hover">
+      <button className="topuserIconBtn">
         <img src={require('./unnamed.png')} alt="user" loading="lazy" decoding="sync" />
       </button>
     </section>
@@ -37,7 +37,7 @@ function TopMenu(props) {
 }
 
 function TopDrawerBtns(props) {
-  let c = props.active ? "drawerBtnActive" : "";
+  let c = props.active ? "topdrawerBtnActive" : "";
   let l = [];
 
   return (
@@ -54,7 +54,7 @@ function TopDrawerBtns(props) {
         });
         props.func(l);
       }
-    } className={c+" drawerBtn"}>{props.name}</button>
+    } className={c+" topdrawerBtn"}>{props.name}</button>
   );
 }
 function TopDrawer(props) {
@@ -72,7 +72,7 @@ function TopDrawer(props) {
       {
         drawerList.map(x => {return <TopDrawerBtns key={x.name} name={x.name} active={x.active} list={drawerList} func={setDrawerList} />})
       }
-      <button className="drawerOptBtn">
+      <button className="topdrawerOptBtn">
         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-three-dots-vertical" viewBox="0 0 16 16">
           <path d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z"/>
         </svg>
@@ -82,7 +82,7 @@ function TopDrawer(props) {
 }
 
 function Top() {
-  const [drawerTop, setDrawerTop] = useState(-5);
+  const [drawerTop, setDrawerTop] = useState(35);
 
   return (
     <div className="top">
